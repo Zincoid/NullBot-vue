475b2082e446b0c112b2396dcd26b4a53f9de8d3
Zincoid <2660181154@qq.com>
2025-12-26 04:03:40 +0800

    NullBot 前端Vue工程

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   lintOnSave: false,
   devServer: {
     //host: '0.0.0.0', // ip
-    port: 7000, // 设置端口号
+    port: 80, // 设置端口号
     https: false, // https:{type:Boolean}
     open: false, //配置自动启动浏览器
     proxy: null  //设置代理
