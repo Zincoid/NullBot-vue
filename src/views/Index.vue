4eaa8648afb07c174a380084086248a78b298d33
Zincoid <2660181154@qq.com>
2026-01-08 21:37:47 +0800

    NullBot 前端Vue工程

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1836,7 +1836,20 @@ export default {
           'token': localStorage.getItem("token")
         },
         method: 'GET'
-      })
+      }).then(res => {
+        const blob = new Blob([res.data]);
+        const elink = document.createElement('a');
+        elink.download = `Items_${new Date().toLocaleString()}.csv`;
+        elink.style.display = 'none';
+        elink.href = URL.createObjectURL(blob);
+        document.body.appendChild(elink);
+        elink.click();
+        URL.revokeObjectURL(elink.href); // 释放URL 对象
+        document.body.removeChild(elink);
+        this.$message.success("导出成功")
+      }).catch(error => {
+        this.$message.error("导出失败")
+      });
     }
   },
 
