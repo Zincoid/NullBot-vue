1e01e54f79bc96834ccf33338b05e4da7bfe9d09
Zincoid <2660181154@qq.com>
2026-05-15 18:59:38 +0800

    modified:   CHANGELOG.md modified:   deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -16,22 +16,27 @@ function Exec {
     }
 }
 
-Write-Host "=== [1/6] Building ===" -ForegroundColor Cyan
+Write-Host "[ === Nginx Deployment === ]" -ForegroundColor Cyan
+
+Write-Host "[ === [1/7] Building === ]" -ForegroundColor Cyan
 Exec "npm run build"
 
-Write-Host "=== [2/6] Packing dist.tar.gz ===" -ForegroundColor Cyan
+Write-Host "[ === [2/7] Packing dist.tar.gz === ]" -ForegroundColor Cyan
 Exec "tar -czvf $TAR dist"
 
-Write-Host "=== [3/6] Uploading to $SERVER`:$REMOTE ===" -ForegroundColor Cyan
+Write-Host "[ === [3/7] Uploading to $SERVER`:$REMOTE === ]" -ForegroundColor Cyan
 Exec "scp $SSH_OPTS $TAR `"$SERVER`:$REMOTE`""
 
-Write-Host "=== [4/6] Removing old dist on server ===" -ForegroundColor Cyan
+Write-Host "[ === [4/7] Removing old dist on server === ]" -ForegroundColor Red
 Exec "ssh $SSH_OPTS $SERVER `"rm -rf $NGINX`""
 
-Write-Host "=== [5/6] Extracting on server ===" -ForegroundColor Cyan
+Write-Host "[ === [5/7] Extracting on server === ]" -ForegroundColor Cyan
 Exec "ssh $SSH_OPTS $SERVER `"sudo tar -xzvf $REMOTE/$TAR -C /usr/share/nginx`""
 
-Write-Host "=== [6/6] Reloading nginx ===" -ForegroundColor Cyan
+Write-Host "[ === [6/7] Reloading nginx === ]" -ForegroundColor Cyan
 Exec "ssh $SSH_OPTS $SERVER `"sudo nginx -s reload`""
 
-Write-Host "=== Done ===" -ForegroundColor Green
+Write-Host "[ === [7/7] Removing local $TAR === ]" -ForegroundColor Red
+Exec "rm $TAR"
+
+Write-Host "[ === Nginx Deployment Done === ]" -ForegroundColor Green
